30d42417c28db58e894f15cad674273a0d8752e6
Michael Phan-Ba <michael@mikepb.com>
2014-09-01 01:12:57 -0700

    Validate checksums on read

--- starfish/messages.go
+++ starfish/messages.go
@@ -53,9 +53,15 @@ func validate(m interface{}) error {
 }
 
 func unmartialBinary(m interface{}, data []byte) error {
+	size := binary.Size(m)
+
 	// check for expected number of bytes
-	if len(data) < binary.Size(m) {
+	if len(data) < size {
 		return InsufficientBytesError
+	}
+	// validate checksum
+	if err := validateChecksum(data, size); err != nil {
+		return err
 	}
 	// unmarshal data
 	if err := binary.Read(bytes.NewBuffer(data), binary.LittleEndian, m); err != nil {
@@ -90,6 +96,26 @@ func martialBinary(m interface{}) ([]byte, error) {
 	}
 
 	return w.Bytes(), nil
+}
+
+func validateChecksum(b []byte, size int) error {
+	var csum int32
+	wc := bytes.NewBuffer(b)
+	nints := int(size/4) - 1
+	var i32 int32
+	for i := 0; i < nints; i++ {
+		if err := binary.Read(wc, binary.LittleEndian, &i32); err != nil {
+			return err
+		}
+		csum += i32
+	}
+	if err := binary.Read(wc, binary.LittleEndian, &i32); err != nil {
+		return err
+	}
+	if csum != i32 {
+		return InvalidMessageError
+	}
+	return nil
 }
 
 func writeChecksum(b *bytes.Buffer) error {
